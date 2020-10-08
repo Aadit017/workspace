5fb08e6d157dc67a664190840997badcc23631e7
Aadit017 <66309635+Aadit017@users.noreply.github.com>
2020-10-08 20:55:33 +0530

    mno

--- .py
+++ .py
@@ -1,12 +1,9 @@
-def one(n): 
-    return [str(x) for x in range(n)]
-def two(n):
-    return list(map(str,range(n)))
-import time as t 
-start_time=t.time()
-result=one(1000000)
-end_time=t.time()
-elapsed_time=end_time - start_time
-print(elapsed_time)
-
- 
+def func(word): 
+    lst=list(word)
+    for i in range(len(lst)): 
+        if i%2==0:
+            lst[i]=lst[i].upper()
+        else: 
+            lst[i]=lst[i]
+    return lst
+print(func('word'))
--- code.py
+++ code.py
@@ -1,12 +1,4 @@
-def one(n): 
-    return [str(x) for x in range(n)]
-def two(n):
-    return list(map(str,range(n)))
-import time as t 
-start_time=t.time()
-result=two(1000)
-end_time=t.time()
-elapsed_time=end_time - start_time
-print(elapsed_time)
-
- 
+import re 
+text='the phone is near the stand'
+pattern='phone'
+print(re.search(pattern,text))
