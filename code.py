a07909757dd1dd835494130138e2c6dae99e5244
Aadit017 <66309635+Aadit017@users.noreply.github.com>
2020-10-09 17:36:01 +0530

    do3nd

--- code.py
+++ code.py
@@ -1,4 +1,8 @@
 import re 
-text='the phone is near the stand'
-pattern='phone'
-print(re.search(pattern,text))
+text=input(" please enter the text :-)")
+try: 
+    phone_pattern=re.compile(r'(\d{3})-(\d{3})-(\d{4})')
+    match=re.search(phone_pattern,text)
+    print(match.group())
+except: 
+    print(" sorry :-(")
