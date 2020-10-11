a2b915891e1f51bf9d089a8d094720b8e5eca589
Aadit017 <66309635+Aadit017@users.noreply.github.com>
2020-10-11 16:53:28 +0530

    done

--- normalcoding/Solution.java
+++ normalcoding/Solution.java
@@ -1,29 +1,35 @@
-package normalcoding;
 
 import java.util.Scanner;
 
 public class Solution {
     public static void main (String args []) { 
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        int min =0,max=0;
-        for (int i =0;i<n;i++){ 
-            arr[i]=sc.nextInt();
+        int x1=sc.nextInt();
+        int v1=sc.nextInt();
+        int x2=sc.nextInt();
+        int v2=sc.nextInt();
+        if (x2>x1) { 
+            if (v2>v1){ 
+System.out.println("NO");
+System.exit(0);
+            }
         }
-        for (int i=0;i<n;i++){ 
-    if (min<arr[i]){ 
-    min=arr[i];
-    System.out.println(min);
+        if(x1>x2){ 
+            if(v1>v2){
+                System.out.println("NO");
+                System.exit(0);
+            }
+        }
+        while(true){ 
+            if (x1==x2){ 
+               System.out.println("YES");
+                System.exit(0);
+            }
+            x1=x1+v1;
+            x2=x2+v2;
+        }
+    
+   
     }
-    if  (max>arr[i]){ 
-     max=arr[i];
-     System.out.println(max);
-    }
-    continue ;
-}
-System.out.println(min+' '+max);
-sc.close();
-} 
-} 
 
+}
