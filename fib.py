ea29649a565c85cb1345f795b5d9741e8b81cf68
Aadit017 <66309635+Aadit017@users.noreply.github.com>
2020-10-13 18:19:53 +0530

    save

--- fib.py
+++ fib.py
@@ -1,6 +1,7 @@
 import requests 
 import bs4 
-res=requests.get('https://www.stocktrak.com/account/openpositions?ClientName=Wharton')
+res=requests.get('https://www.stocktrak.com/trading/equities?ClientName=Wharton')
 soup=bs4.BeautifulSoup(res.text,'lxml')
-site=soup.select('profit-loss plus text-right')
-print(site[0].getText())
+site=soup.select('.item up>span')
+for i in site: 
+    print(i.getText())
--- practice/webscrape_prc.py
+++ practice/webscrape_prc.py
@@ -2,5 +2,6 @@ import requests
 import bs4
 res=requests.get("https://www.stocktrak.com/dashboard?ClientName=Wharton")
 soup=bs4.BeautifulSoup(res.text,'lxml')
-site=soup.select('title')
-print(site)
+site=soup.select('.count currency')
+for item in site: 
+    print(item.text)
