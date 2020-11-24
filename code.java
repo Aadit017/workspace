13d265878ffd06653adda813dff1934d63b3dc4e
Aadit <66309635+Aadit017@users.noreply.github.com>
2020-11-24 10:30:51 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,10 @@
+public class code{ 
+    public static void main(String args[]){ 
+        int a=5,b=7;
+        a=b++;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("guess what will a+b be---->"+ (a+b));
+        }
+}
